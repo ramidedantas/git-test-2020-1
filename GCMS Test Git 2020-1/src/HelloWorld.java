e4b3e8552fdb86ceb7ab0751ee2634d14c5f4dbf
Ramide Dantas <ramidedantas@recife.ufpe.edu.br>
2020-09-16 11:26:57 -0300

    Commit bash master

--- GCMS Test Git 2020-1/src/HelloWorld.java
+++ GCMS Test Git 2020-1/src/HelloWorld.java
@@ -2,5 +2,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world!!! - BASH+Eclipse - Merged!");
+
+		System.out.println("Bash - master!");		
 	}
 }
