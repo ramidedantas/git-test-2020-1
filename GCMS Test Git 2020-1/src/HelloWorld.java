13b35ae74c2111b78c84b31357c3b20c5a588dff
Ramide Dantas <ramidedantas@recife.ufpe.edu.br>
2020-09-16 11:09:23 -0300

    Branch Eclipse 2

--- GCMS Test Git 2020-1/src/HelloWorld.java
+++ GCMS Test Git 2020-1/src/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world!!! - BASH+Eclipse");
 		
-		System.out.println("Branch Eclipse!");
+		System.out.println("Branch Eclipse!!!");
 	}
 }
